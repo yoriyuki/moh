e13d1fb603e5d97fd8b1d386b63a618bdcce0f00
yoriyuki <yoriyuki.y@gmail.com>
2015-09-23 20:42:10 +0900

    support dayone, defautl suffix=txt

--- moh.rb
+++ moh.rb
@@ -16,8 +16,8 @@ class Transaction
 
   private
   #amount is plus when the money moves from [source] to [target]
-  attr_writer :date, :source, :target, :sort, :amount, :comment 
-  public 
+  attr_writer :date, :source, :target, :sort, :amount, :comment
+  public
   attr_reader :date, :source, :target, :sort, :amount, :comment
 
   # amount > 0
@@ -33,13 +33,13 @@ class Transaction
       self.target = source
       self.amount = -amount
     end
-    if not self.source then p amount end 
+    if not self.source then p amount end
       @@pool << self
   end
 
   def <=>(other)
     (self.date <=> other.date) != 0 ? self.date <=> other.date :
-      (self.target <=> other.target) != 0 ? self.target <=> other.target : 
+      (self.target <=> other.target) != 0 ? self.target <=> other.target :
       (self.amount <=> other.amount) != 0 ? self.amount <=> other.amount :
       self.comment <=> other.comment
   end
@@ -50,9 +50,9 @@ class Transaction
 
 end
 
-def initial_of(path1, path2) 
-  if path1 == [] then 
-    return true 
+def initial_of(path1, path2)
+  if path1 == [] then
+    return true
   elsif path2 == [] then
     return false
   elsif path1[0] == path2[0] then
@@ -72,8 +72,8 @@ class Book
   attr_reader :name
 
   def add_child(book)
-    if self.children then 
-      self.children[book.name] = book 
+    if self.children then
+      self.children[book.name] = book
     else
       self.children = {book.name => book}
     end
@@ -96,7 +96,7 @@ class Book
   def full_path
     if parent then
       parent.full_path << self.name
-    else 
+    else
       self.name.length == 0 ? [] : [self.name]
     end
   end
@@ -109,14 +109,14 @@ class Book
     if path.length > 0 then
       path = path.dup
       name = path.shift
-      get_child(name) ? get_child(name).get_grandchild(path, create) : 
+      get_child(name) ? get_child(name).get_grandchild(path, create) :
         (create ? Book.new(name, self).get_grandchild(path, create) : nil)
     else
       self
     end
   end
 
-  def <=>(other) 
+  def <=>(other)
     full_path <=> other.full_path
   end
 
@@ -155,6 +155,7 @@ end
 
 ##Input
 LINE_START = /^\[(\d{4}-\d{2}-\d{2})\]\$\s+/
+DAYONE_LINE = /^(.*<string>)?(.*)(<\/string>.*)?$/
 
 class BookReader
   protected
@@ -176,22 +177,29 @@ class BookReader
         t = Transaction.new(date, book1, book2, amount, "")
       end
       true
-      
+
       rescue ArgumentError
       false
       end
     end
 
-  def parse_line(line) 
+  def parse_line(line)
     md = LINE_START.match(line)
     if md then
       entries = line.chomp.split(/\s+/)
-      if entries.length >= 4 then 
-        add_line(md[1], 
+      if entries.length >= 4 then
+        add_line(md[1],
                  entries[1].split(':'),
                  entries[2].split(':'),
                  entries[-1].to_i)
       end
+    end
+  end
+
+  def parse_dayone_line(line)
+    md = DAYONE_LINE.match(line)
+    if md then
+      parse_line(md[2])
     end
   end
 end
@@ -213,10 +221,10 @@ class BookWriter
     self.book = book
   end
 
-  def print_summary(date1, date2)
+  def print_summary(date1, date2, depth=-1)
     debit = 0
     Transaction.each do |t|
-      if 
+      if
           t.is_in_dates(date1, date2)    \
         and t.source.is_contained(book)
       then
@@ -225,7 +233,7 @@ class BookWriter
     end
     credit = 0
     Transaction.each do |t|
-      if 
+      if
           t.is_in_dates(date1, date2)    \
         and t.target.is_contained(book)
       then
@@ -235,13 +243,15 @@ class BookWriter
     if credit != 0 or debit != 0 then
       puts "#{book.fqdn}\t#{credit}\t#{debit}\t#{credit - debit}"
     end
-    book.get_children.sort.each{|b| BookWriter.new(b).print_summary(date1, date2)}
+    if depth != 0 then
+      book.get_children.sort.each{|b| BookWriter.new(b).print_summary(date1, date2, depth-1)}
+    end
   end
 
   def print_transactions(date1=nil, date2=nil)
     transactions = []
     Transaction.each do |t|
-      if 
+      if
           t.is_in_dates(date1, date2)    \
         and (t.source.is_contained(book) or t.target.is_contained(book))
       then
@@ -252,56 +262,68 @@ class BookWriter
   end
 end
 
-
 ## Main
 if $0 == __FILE__ then
 puts 'moh -- simple, commandline-based accounting software'
-puts '(C) 2012, 2013, 2014: Yoriyuki Yamagata'
+puts '(C) 2012, 2013, 2014, 2015: Yoriyuki Yamagata'
 puts 'See LICENSE.txt for the licence.'
 opt = OptionParser.new
 book_reader = BookReader.new
-howm_dir = nil
-howm_suffix = 'howm'
+txt_dir = nil
+txt_suffix = 'txt'
 print_summary = false
+summary_depth = -1
 print_transactions = false
-Version = "0.2.0"
+Version = "0.3.0"
 
-opt.on('-d [dir]'){|dir| howm_dir = dir}
-opt.on('--howm_suffix=[suffix]'){ |suffix| howm_suffix=suffix }
+def dir_scanner(path, suffix)
+  Dir.glob("#{File.expand_path(path)}/**{,/*/**}/*.#{suffix}") {|path| yield(path)}
+end
+
+opt.on('-d dir'){|dir|
+  dir_scanner(dir, txt_suffix) do |path|
+    File.open(path){ |file| file.each{|line|
+        line.force_encoding('ascii-8bit')
+        book_reader.parse_line(line)
+        }}
+  end
+}
+
+opt.on('--dayone=dir'){|dir|
+  dir_scanner(dir, 'doentry') do |path|
+    File.open(path){ |file| file.each{|line|
+        line.force_encoding('ascii-8bit')
+        book_reader.parse_dayone_line(line)
+        }}
+  end
+}
+
+opt.on("-D depth"){|depth| summary_depth = Integer(depth) - 1}
+opt.on('--txt_suffix=suffix'){ |suffix| txt_suffix=suffix }
 opt.on('-s', '--summary'){ |b| print_summary = true }
 opt.on('-t', '--transactions'){ |b| print_transactions = true }
 
 values = []
 opt.order!{ |v| values << v }
 
-def dir_scanner(path, suffix)
-  Dir.glob("#{File.expand_path(path)}/**{,/*/**}/*.#{suffix}") {|path| yield(path)}
-end
-
-if howm_dir then 
-  dir_scanner(howm_dir, howm_suffix) do |path|
-    File.open(path){ |file| file.each{|line| 
-        line.force_encoding('ascii-8bit')
-        book_reader.parse_line(line)
-        }}
-  end
-end
-
 if values.length == 3 then
   bookname = values[0].dup
   bookname.force_encoding('ascii-8bit')
   book = book_reader.root_book.get_grandchild(bookname.split(':'))
   book_writer = BookWriter.new(book)
-end
 
-if print_summary then
-  puts '*Summary'
-  book_writer.print_summary(Date.parse(values[1]), Date.parse(values[2]))
-end
+  if print_summary then
+    puts '*Summary'
+    book_writer.print_summary(Date.parse(values[1]), Date.parse(values[2]), summary_depth)
+  end
 
-if print_transactions then
-  puts '*Transactions'
-  book_writer.print_transactions(Date.parse(values[1]), Date.parse(values[2]))
+  if print_transactions then
+    puts '*Transactions'
+    book_writer.print_transactions(Date.parse(values[1]), Date.parse(values[2]))
+  end
+
+else
+   puts "Too few arguments\n"
 end
 
 end
